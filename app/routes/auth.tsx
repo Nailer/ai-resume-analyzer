140250c2cbcbd735651829599b1e569a6eb2f568
Sukuna <ajeemmanuel221@gmail.com>
2025-09-30 18:51:31 +0100

    added the puter types and configuration [started the authe page]

---
+++ app/routes/auth.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const meta = () => ([
+    { title: "Resumind | Auth" },
+    { name: "description", content: "Log into your account" }
+])
+
+const auth = () => {
+  return (
+    <main className="bg-[url('public/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
+        <div className='gradient-border shadow-lg'>
+            {/* <section className='flex flex-col gap-8 bg-white rounded-2xl p-10'>
+                <div className='flex flex-col items-center gap-2 text-center'>
+                    <h1>Welcome</h1>
+                    <h2>Log In to Continue Your Job Journey</h2>
+                </div>
+            </section> */}
+        </div>
+    </main>
+  )
+}
+
+export default auth
